29811aee5aee5562f5a3511e60c115c9c277dd21
Misaelprg <mrkenkkenk@gmail.com>
2024-04-16 10:22:00 -0600

    Mi Primer Microservicio

--- src/main/java/com/fcfm/backend/controller/AlumnoApiController.java
+++ src/main/java/com/fcfm/backend/controller/AlumnoApiController.java
@@ -2,12 +2,16 @@ package com.fcfm.backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.fcfm.backend.model.Alumno;
 
 @RequestMapping("/alumno")
 public interface AlumnoApiController {
     @GetMapping("/")
     ResponseEntity<String> getName();
 
-    @GetMapping("/{nombreAlumno}")
+    @GetMapping({"/{nombreAlumno}"})
     ResponseEntity<String> getName(@PathVariable String nombreAlumno);
+
+    @PostMapping("/")
+    ResponseEntity<Alumno> createAlumno(@RequestBody Alumno alumnoNuevo);
 }
